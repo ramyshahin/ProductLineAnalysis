70affa051eb8fac9dd449f8107be724176ee5408
Ramy Shahin <r.shahin@computer.org>
2016-08-14 21:08:12 -0400

    Lifting functions (in progress)

--- LiftedLTS.hs
+++ LiftedLTS.hs
@@ -9,23 +9,18 @@ import Data.List
 import Data.Tree
 import Variability
 
--- abstract State type
+-- lifted State type
 type StateLifted = Lifted State
 
+-- lifted Transition type
 type TransitionLifted = Lifted Transition
 
+-- lifted Transition constructor
 consLiftedTransition :: StateLifted -> StateLifted -> Act -> PresenceCondition -> TransitionLifted
 consLiftedTransition s0 s1 a pc = apply3 pc Transition s0 s1 (liftValue a)
 
-{-
-getSource :: TransitionLifted -> StateLifted
-getSource t = [(s0,pc) | ((s0,_,_),pc) <- t]
-
-getTarget :: TransitionLifted -> StateLifted
-getSource t = [(s1,pc) | ((_,s1,_),pc) <- t]
-
-getAction :: TransitionLifted -> StateLifted
--}
+-- lifted neighbors function
+-- neighbors ts s = [(target t) | t <- ts, (source t) == s]
 
 neighborsLifted :: [TransitionLifted] -> StateLifted -> [StateLifted]
-neighborsLifted ts s = [(target t) | t <- ts, apply2 (==) (source t) s]
+neighborsLifted ts s = [map (\(t',pc) -> (target t',pc)) (filter (\(t',_) -> (source t' == s)) t) | t <- ts]
