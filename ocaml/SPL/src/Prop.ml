078760400c115ed776f3397e80624ffa40429d4e
Ramy Shahin <r.shahin@computer.org>
2017-09-30 22:35:47 -0400

    lifting unary functions

---
+++ ocaml/SPL/src/Prop.ml
@@ -0,0 +1,21 @@
+open MLBDD
+
+let mgr = MLBDD.init ~cache:100 ()
+
+type presenceCondition = MLBDD.t
+
+let tt = dtrue mgr
+let ff = dfalse mgr
+
+let atom id = ithvar mgr id
+let neg = dnot
+let conj pc1 pc2 = dand pc1 pc2
+let disj pc1 pc2 = dor pc1 pc2
+let impl pc1 pc2 = imply pc1 pc2
+
+let unsat = is_false
+
+let sat = (fun x -> (not (unsat x)))
+
+
+
--- ocaml/SPL/src/SPL.ml
+++ ocaml/SPL/src/SPL.ml
@@ -3,23 +3,33 @@
 module SPL*)
 
 open List
-
-type presenceCondition = int
-
-let conj pc1 pc2 = pc1 + pc2
-
-let sat pc = pc != 0
+open Prop
 
 type 'a atom = 'a * presenceCondition
 
 type 'a var = ('a atom) list
 
-let mkVar v p = [(v,p)]
+let mkVar (v: 'a) (p: presenceCondition) : 'a var = [(v,p)]
+let mkVarT v = mkVar v tt
 
-let apply fn x = 
+let apply (fn : ('a -> 'b) var) (x : 'a var) : 'b var = 
   let allPairs = List.concat (List.map(fun (fn',fnpc') -> List.map (fun (x',xpc') -> (fn' x', conj fnpc' xpc')) x) fn)
   in  filter (fun (x,xpc) -> sat xpc) allPairs
 
-let a = mkVar 'a' 1
-let b = mkVar 'b' 2
-let c = mkVar 'c' 3
+let liftedNeg = mkVarT (~-)
+
+(* presence conditions *)
+let p_a = atom 1
+let p_b = atom 2
+let n_a = neg p_a
+let n_b = neg p_b
+let p_a_and_p_b = conj p_a p_b
+let p_a_and_n_b = conj p_a n_b
+let n_a_and_p_b = conj n_a p_b
+let n_a_and_n_b = conj n_a n_b
+
+let u = [(18, p_a_and_p_b); (12, p_a_and_n_b); (0, n_a_and_p_b); (3, n_a_and_n_b)]
+let v = [(-5, p_a_and_p_b); (9, p_a_and_n_b); (2, n_a_and_p_b); (8, n_a_and_n_b)]
+let w = [(1, p_a_and_p_b); (6, p_a_and_n_b); (10, n_a)]
+let x = [(7,p_a); (11, n_a)]
+let y = [(13, p_a_and_p_b); (-23, n_a_and_p_b); (20, n_b)]
