b166c5aa474f1bce835406d4573c9ef79be150a1
Ramy Shahin <r.shahin@computer.org>
2019-05-28 04:46:05 -0400

    HelloRefactor example

---
+++ haskell/rewriter/src/HelloRefactor.hs
@@ -0,0 +1,18 @@
+module HelloRefactor where
+
+import Language.Haskell.Tools.PrettyPrint (prettyPrint)
+import Language.Haskell.Tools.Refactor
+
+import Control.Reference ((.-))
+import Debug.Trace (trace)
+import SrcLoc (RealSrcSpan)
+
+tryItOut :: String -> String -> IO ()
+tryItOut = tryRefactor (localRefactoring . helloRefactor)
+
+helloRefactor :: RealSrcSpan -> LocalRefactoring
+helloRefactor sp = return . (nodesContained sp .- helloExpr)
+
+helloExpr :: Expr -> Expr
+helloExpr e = trace ("\n### Hello: " ++ prettyPrint e) $ e
+
---
+++ haskell/rewriter/test/Test.hs
@@ -0,0 +1,7 @@
+module Test where
+
+f = id
+g = id
+
+x = f (f (g 2))
+
