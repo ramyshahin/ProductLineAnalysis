49b97c10c8f8e458a66d696ca946bf5cbb30ce9b
Ramy Shahin <r.shahin@computer.org>
2019-06-16 12:59:23 -0400

    premable modifications

--- haskell/ghcRewriter/src/GhcRewriter.hs
+++ haskell/ghcRewriter/src/GhcRewriter.hs
@@ -1,15 +1,43 @@
 module GhcRewriter(plugin) where
+
 import GhcPlugins
+import HsSyn
 
 plugin :: Plugin
 plugin = defaultPlugin {
-  installCoreToDos = install
+  installCoreToDos = coreToDos,
+  parsedResultAction = modifyPreamble
   }
   
-install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
-install _ todo = do
+modifyPreamble :: [CommandLineOption] -> ModSummary -> HsParsedModule -> Hsc HsParsedModule
+modifyPreamble _ _ m = (renameModule m) >>= addDependencies
+
+coreToDos :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
+coreToDos _ todo = do
   putMsgS "Hello!"
-  return (CoreDoPluginPass "Say name" pass : todo)
+  return ((CoreDoPluginPass "Say name" pass) : todo)
+
+-- does this actually rename the module? do we also need to rename the source file?
+renameModule :: HsParsedModule -> Hsc HsParsedModule
+renameModule m =
+  let hsMod@(L s mod)       = hpm_module m
+      (Just (L x modName))  = hsmodName mod
+      name  flags           = showSDoc flags (ppr modName)
+      name' flags           = mkModuleName $ (name flags) ++ "Deep"
+      mod' flags            = m { hpm_module = L s (mod { hsmodName = Just (L x (name' flags))})}
+  in  do
+    flags <- getDynFlags
+    return $ mod' flags
+
+addDependencies :: HsParsedModule -> Hsc HsParsedModule
+addDependencies m = 
+  let hsMod@(L s mod) = hpm_module m
+      imports         = hsmodImports mod
+      splImport       = simpleImportDecl (mkModuleName "SPL")
+      imports'        = (L noSrcSpan splImport) : imports
+      mod'            = m { hpm_module = L s (mod { hsmodImports = imports'}) }
+  in  do
+    return mod'
 
 pass :: ModGuts -> CoreM ModGuts
 pass mod = bindsOnlyPass (mapM printBind) mod
