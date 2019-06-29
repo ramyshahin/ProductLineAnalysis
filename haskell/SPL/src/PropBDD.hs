15873eebd1186f08c2e65413656a050be882d35c
Ramy Shahin <r.shahin@computer.org>
2019-06-28 21:31:11 -0400

    adding textual representation of BDDs

--- haskell/SPL/src/PropBDD.hs
+++ haskell/SPL/src/PropBDD.hs
@@ -1,42 +1,124 @@
 -- PropBDD.hs
 -- Ramy Shahin
 -- May 7th 2017
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE BangPatterns #-}
 module PropBDD where
 
 import Cudd.Cudd
+import Data.Hashable
+import qualified Data.HashTable.IO as H
+import Control.Monad.ST
+import qualified Data.List as L 
+import Debug.Trace
+import Data.STRef
+import Control.Monad
+import System.IO.Unsafe
 
-manager = cuddInit
+type HashTable k v = H.BasicHashTable k v
 
-type Prop = DDNode
+instance Hashable DDNode where
+    hashWithSalt s d = hashWithSalt s (show d)
+
+data Prop =
+    TT
+  | FF
+  | Atom String
+  | Neg Prop
+  | And Prop Prop
+  | Or Prop Prop    
+    deriving (Eq) 
+
+instance Show Prop where 
+    show p = case p of 
+        TT -> "True"
+        FF -> "False"
+        Atom s -> s -- do 
+                    --let str = unsafePerformIO $ H.lookup prop2str p
+                    --let l = unsafePerformIO $ htSize prop2str
+                    --case str of
+                    --    Just s -> s
+                    --    _ -> trace ("Mismatch! size: " ++ (show l)) (show d)
+        Neg p' -> "~" ++ (show p')
+        And p1 p2 -> "(" ++ (show p1) ++ " /\\ " ++ (show p2) ++ ")"
+        Or  p1 p2 -> "(" ++ (show p1) ++ " \\/ " ++ (show p2) ++ ")"
+
+instance Hashable Prop where
+    hashWithSalt s p = hashWithSalt s (show p)
+
 type Universe = [Prop]
 
+prop2bdd :: HashTable Prop DDNode
+bdd2prop :: HashTable DDNode Prop
+
+htSize :: (Eq k, Hashable k) => H.BasicHashTable k v -> IO Int 
+htSize h = do
+    xs <- H.toList h 
+    return $ length xs 
+    
+manager = cuddInit
+prop2bdd = unsafePerformIO H.new 
+bdd2prop = unsafePerformIO H.new 
+
+newBDD :: Prop -> DDNode -> Prop
+newBDD p d = unsafePerformIO $ do
+    p' <- H.lookup bdd2prop d
+    case p' of 
+            Nothing -> do
+                        !d0 <- H.insert bdd2prop d p
+                        !d1 <- H.insert prop2bdd p d
+                        --trace ("inserted: " ++ (show p)) (return p)
+                        return p
+            Just p'' -> return p''   
+
+mkBDDVar :: String -> Int -> Prop
+mkBDDVar name index = 
+    let r = ithVar manager index
+    in newBDD (Atom name) r
+
 mkUniverse :: [String] -> Universe
-mkUniverse xs = map (\(s,r) -> (ithVar manager r)) (zip xs [0..])
-
-queryOrUpdate :: Universe -> String -> (Universe, Prop)
-queryOrUpdate [] s = (u', p)
-    where   u' = mkUniverse [s]
-            p  = head u' 
-
---queryOrUpdate u@((s',p'):u') s =
---    if s == s' 
---    then (u,p')
---    else ((s',p'):u'', p'')
---         where (u'',p'') = queryOrUpdate u' s
-         
+mkUniverse xs = map (\(s,r) -> mkBDDVar s r) (zip xs [0..])  
+    
 {-# NOINLINE tt #-}
-tt = readOne manager
-ff = readLogicZero manager
+tt = newBDD TT (readOne manager)
+ff = newBDD FF (readLogicZero manager)
 
-neg = bNot manager
+andBDD :: Prop -> Prop -> Prop
+andBDD a b =
+    let a' = unsafePerformIO $ H.lookup prop2bdd a 
+        b' = unsafePerformIO $ H.lookup prop2bdd b
+    in  case (a', b') of 
+        (Just a'', Just b'') -> newBDD (And a b) $ bAnd manager a'' b''
+        (Just _, Nothing) -> error $ "Can not find RHS: " ++ (show b)
+        (Nothing, Just _) -> error $ "Can not find LHS: " ++ (show a)
+        _ -> error $ "Can not find props: " ++ (show a) ++ " " ++ (show b)
+
+orBDD :: Prop -> Prop -> Prop
+orBDD a b = 
+    let a' = unsafePerformIO $ H.lookup prop2bdd a 
+        b' = unsafePerformIO $ H.lookup prop2bdd b
+    in  case (a', b') of 
+        (Just a'', Just b'') -> newBDD (Or a b) $ bOr manager a'' b''
+        _ -> error $ "Can not find props: " ++ (show a) ++ " " ++ (show b)
+
+notBDD :: Prop -> Prop 
+notBDD a = 
+    let a' = unsafePerformIO $ H.lookup prop2bdd a 
+    in  case a' of 
+        Just a' -> newBDD (Neg a) $ bNot manager a'
+        _ -> error $ "Can not find prop: " ++ (show a)
+
+neg :: Prop -> Prop 
+neg = notBDD 
 
 conj :: [Prop] -> Prop
-conj =  foldr (bAnd manager) tt
+conj =  foldl andBDD tt
 
 disj :: [Prop] -> Prop
-disj = foldr (bOr manager) ff
+disj = foldr orBDD ff
 
-impl p q = disj[neg p, q] 
+impl p q = disj[notBDD p, q] 
 
 tautology :: Prop -> Bool
 tautology p = p == tt
--- haskell/SPL/test/Prop_test.hs
+++ haskell/SPL/test/Prop_test.hs
@@ -1,35 +1,35 @@
 -- Propositional Logic and SAT solving Tests
 -- Ramy Shahin
 -- Jan 3rd 2017
-import Prop
-
+import PropBDD
+import Control.Monad.ST
 import GHC.Arr
 
-u :: Universe
-u = mkUniverse ["P", "Q", "R", "S"]
-
 p, q, r, s :: Prop
-p = Atom u 0
-q = Atom u 1
-r = Atom u 2
-s = Atom u 3
+u :: Universe
+u@[p, q, r, s] = mkUniverse ["P", "Q", "R", "S"]
+
+--p = Atom u 0
+--q = Atom u 1
+--r = Atom u 2
+--s = Atom u 3
 
 prop1 = conj [p, (neg q)]   -- sat
 prop2 = disj [(neg r), s]   -- sat
 prop3 = conj [p, (neg p)]   -- unsat
 prop4 = disj [r]            -- sat
-prop5 = conj [F, p, q]      -- unsat
-prop6 = disj [T, prop1, prop3] -- sat
+prop5 = conj [ff, p, q]      -- unsat
+prop6 = disj [tt, prop1, prop3] -- sat
 prop7 = impl p (neg p)      -- sat
-prop8 = iff  p (neg p)      -- unsat
-prop9 = impl p q            -- sat
-prop10 = iff p q            -- sat
+--prop8 = iff  p (neg p)      -- unsat
+--prop9 = impl p q            -- sat
+--prop10 = iff p q            -- sat
 
-props = [prop1, prop2, prop3, prop4, prop5, prop6, prop7, prop8, prop9, prop10]
+props = [prop1, prop2, prop3, prop4, prop5, prop6, prop7] --, prop8, prop9, prop10]
 
 run :: IO ()
 run = mapM_ (\p -> do
-                    let r = checkSAT p
+                    let r = sat p
                     putStrLn ((show p) ++ "------>" ++ (show r))
             ) props
 
