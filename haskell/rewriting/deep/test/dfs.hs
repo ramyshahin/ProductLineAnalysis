e4d1d8e8906000ef87495cc05f7423799786d859
Ramy Shahin <r.shahin@computer.org>
2020-04-16 04:36:44 -0400

    ONGOING: DFS test

---
+++ haskell/rewriting/deep/test/dfs.hs
@@ -0,0 +1,12 @@
+module dfs where
+import Data.List
+
+post :: [(Int,Int)] -> Int -> [Int]
+post edges v = 
+  if   empty edges
+  then []
+  else let h = head edges in
+       if   fst h == v
+       then fst h :: post (tail edges) v
+       else          post (tail edges) v
+
