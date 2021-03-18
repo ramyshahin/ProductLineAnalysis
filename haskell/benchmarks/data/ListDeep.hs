ae05883fbafceb4307ea1795762fd7f244932318
Ramy Shahin <r.shahin@computer.org>
2021-03-18 22:29:14 +0200

    working on lifting recursive types

--- haskell/benchmarks/data/ListDeep.hs
+++ haskell/benchmarks/data/ListDeep.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE TypeFamilies #-}
 module ListDeep where
 import SPL
 import VPreludeDeep
 import Debug.Trace
+import List(List)
 
-data List a = Nil | Cns (Var a) (List a)
+type family Lifted t :: *
 
-type (List_ a) = Var (List a)
-_Nil = mkVarT $ Nil
-_Cns = mkVarT $ Cns
+data List_ a = List_ { f_Nil :: (I_Nil a), f_Cns :: (I_Cns a) }
+d_List_ = List_ d_Nil d_Cns
+
+type instance Lifted (List a) = List_ a
+
+data I_Nil a = I_Nil
+data I_Cns a = I_Cns (Var a) (List_ a)
+d_Nil = I_Nil
+d_Cns = I_Cns (Var []) d_List_ --(Var [])
+c_Nil = List_ (I_Nil) d_Cns
+c_Cns v0 v1 = List_ d_Nil (I_Cns v0 v1)
 len :: List_ a -> Int_
-len l  = let case0 __cntxt__ = (0 ^| __cntxt__)
-             split0 __dummy__ = case __dummy__ of Nil -> ()
-             case1 __cntxt__ x xs = ((+) ^| __cntxt__) <*> ((1 ^| __cntxt__)) <*> (len (xs /^ __cntxt__))
-             split1 __dummy__ = case __dummy__ of Cns x xs -> (x, xs) in liftedCase (l) (\__dummy__ -> case __dummy__ of Nil -> 0
-                                                                                                                         Cns x xs -> 1) [\__cntxt__ -> (uncurry0 (case0 __cntxt__)) . (liftV split0), \__cntxt__ -> (uncurry2 (case1 __cntxt__)) . (liftV split1)]
+len l  = unions [(\(I_Nil) -> (0 ^| ttPC)) (f_Nil (l)), (\(I_Cns x xs) -> ((+) ^| ttPC) <*> ((1 ^| ttPC)) <*> (len (xs))) (f_Cns (l))]
 
